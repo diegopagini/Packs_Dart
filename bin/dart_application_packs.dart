7844d48407de1bc40732e882925422593a38214a
Diego Pagini <dhpagini@knowmadmood.com>
2023-12-26 14:26:03 +0100

    init

---
+++ bin/dart_application_packs.dart
@@ -0,0 +1,6 @@
+import 'package:dart_application_packs/dart_application_packs.dart'
+    as dart_application_packs;
+
+void main() {
+  print('Hello world: ${dart_application_packs.calculate()}!');
+}
